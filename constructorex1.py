6e4aef4591f4044022f23928cde1aabf00e54289
unknown <subhani16313@gmail.com>
2022-11-06 21:07:16 +0530

    my first file

---
+++ constructorex1.py
@@ -0,0 +1,20 @@
+#program to define constructor in python class
+class A:
+  '''class-A definition'''
+def __init__(self):
+      print("class-A constructor is called");
+      self.x=int(input("enter x value::"));
+      self.y=int(input("enter y value::"));
+      
+def show(self):
+     print(self.x,self.y);
+     
+     
+#obj-creations
+obj1=A()
+obj2=A()
+obj3=A()
+
+print();
+print("obj1-data");
+obj1-A.show(self);     
